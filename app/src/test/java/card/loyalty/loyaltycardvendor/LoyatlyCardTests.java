84cd6185342b72f506eb1cc5e46dd41f866c9482
Sam <sponge3k@gmail.com>
2017-05-07 10:46:48 +1200

    Add additional tests

--- app/src/test/java/card/loyalty/loyaltycardvendor/LoyatlyCardTests.java
+++ app/src/test/java/card/loyalty/loyaltycardvendor/LoyatlyCardTests.java
@@ -19,4 +19,20 @@ public class LoyatlyCardTests {
         card.addToPurchaseCount(5);
         Assert.assertEquals(card.purchaseCount, "5");
     }
+
+    // tests the addToPurchaseCount method with negative number
+    @Test
+    public void testAddToPurchaseCountNegative() {
+        LoyaltyCard card = new LoyaltyCard("Test", "Test");
+        card.addToPurchaseCount(-20);
+        Assert.assertEquals(card.purchaseCount, "-20");
+    }
+
+    // tests the addToPurchaseCount method with a large number
+    @Test
+    public void testAddToPurchaseCountLargeNumber() {
+        LoyaltyCard card = new LoyaltyCard("Test", "Test");
+        card.addToPurchaseCount(999999999);
+        Assert.assertEquals(card.purchaseCount, "999999999");
+    }
 }
